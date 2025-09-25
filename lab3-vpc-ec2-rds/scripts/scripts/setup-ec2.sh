40ef43f091fcfe14cf0cabe1d628017835c79bd9
Ferramelo <carmelo.ferraro991@gmail.com>
2025-09-25 16:44:19 +0200

    Delete lab3-vpc-ec2-rds/scripts/scripts directory

--- lab3-vpc-ec2-rds/scripts/scripts/setup-ec2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# =====================================================
-# Script: setup-ec2.sh
-# Scopo: configurare EC2 per il Lab 3 VPC + EC2 + RDS
-# =====================================================
-
-# --- Inserisci qui i comandi per installare pacchetti, configurare firewall ecc. ---
