4c59794a9f9bfd731dabe6653e84161ee6b25eab
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-03 16:03:52 +0200

    Update backend.tf

--- lab3-vpc-ec2-rds/backend.tf
+++ lab3-vpc-ec2-rds/backend.tf
@@ -1,1 +1,9 @@
 
+terraform {
+  backend "s3" {
+    bucket         = "lab3-terraform-state"
+    key            = "lab3-vpc-ec2-rds/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
