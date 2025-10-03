234181a60e4510e3ffd96d056c6567f910be073a
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-03 16:16:11 +0200

    Delete lab3-vpc-ec2-rds/scripts/test-rds-connection.sh

--- lab3-vpc-ec2-rds/scripts/test-rds-connection.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# =====================================================
-# Script: test-rds-connection.sh
-# Scopo: testare la connessione al database RDS dal server EC2
-# =====================================================
-
-# --- Inserisci qui i comandi per test connessione MySQL/Postgres ---
-
-# Ricorda di rendere eseguibili gli script: chmod +x scripts/*.sh 
