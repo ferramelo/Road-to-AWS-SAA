2a47ddd19c74adfa11dbdf58dc5758680aba90f5
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-03 15:59:35 +0200

    Create backend.tf

---
+++ lab3-vpc-ec2-rds/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "lab3-terraform-state"
+    key            = "lab3-vpc-ec2-rds/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
