faa00221ab98e5726791000d44ca21c22fb3855b
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-03 16:01:02 +0200

    Delete lab3-vpc-ec2-rds/backend.tf

--- lab3-vpc-ec2-rds/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "lab3-terraform-state"
-    key            = "lab3-vpc-ec2-rds/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
