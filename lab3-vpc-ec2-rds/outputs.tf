7ea24f030dc746bac01d66788d66102f659904d4
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-03 16:07:27 +0200

    Create outputs.tf

---
+++ lab3-vpc-ec2-rds/outputs.tf
@@ -0,0 +1,9 @@
+output "bastion_public_ip" {
+  description = "IP pubblico del Bastion Host"
+  value       = aws_instance.bastion.public_ip
+}
+
+output "rds_endpoint" {
+  description = "Endpoint del database RDS"
+  value       = aws_db_instance.mysql.endpoint
+}
