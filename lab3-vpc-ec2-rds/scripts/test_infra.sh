331ac2a784a7bdc9867a34018190c53ba8aeda16
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-06 11:56:34 +0200

    Update test_infra.sh

--- lab3-vpc-ec2-rds/scripts/test_infra.sh
+++ lab3-vpc-ec2-rds/scripts/test_infra.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Script rapido per testare l'infrastruttura creata
+# Test rapido infrastruttura EC2 + RDS
+
+# Recupera output Terraform
+BASTION_IP=$(terraform output -raw bastion_public_ip)
+RDS_ENDPOINT=$(terraform output -raw rds_endpoint)
 
 echo "Verifica EC2 Bastion..."
-BASTION_IP=$1
 ping -c 4 $BASTION_IP
 
-echo "Verifica RDS Endpoint..."
-RDS_ENDPOINT=$2
+echo "Verifica RDS Endpoint (porta 3306)..."
 nc -zv $RDS_ENDPOINT 3306
