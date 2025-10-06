b69483242268e946584bac13b8a3a0d16aa2d3fe
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-06 11:39:20 +0200

    Update ec2.tf

--- lab3-vpc-ec2-rds/ec2.tf
+++ lab3-vpc-ec2-rds/ec2.tf
@@ -1,9 +1,12 @@
 resource "aws_instance" "bastion" {
-  ami           = var.bastion_ami
-  instance_type = var.bastion_instance_type
-  subnet_id     = aws_subnet.public.id
-  security_groups = [aws_security_group.bastion_sg.name]
-  key_name = "lab3-key"
+  ami                    = var.bastion_ami
+  instance_type          = var.bastion_instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  key_name               = "lab3-key"
+
+  associate_public_ip_address = true
 
   tags = { Name = "BastionHost" }
 }
+
