09d6bea0c0d4a0be1677bd4098d01ccc6391aaba
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-06 11:46:17 +0200

    Update security.tf

--- lab3-vpc-ec2-rds/security.tf
+++ lab3-vpc-ec2-rds/security.tf
@@ -1,3 +1,4 @@
+# Bastion Security Group
 resource "aws_security_group" "bastion_sg" {
   name        = "bastion-sg"
   description = "Permette SSH al Bastion Host"
@@ -7,7 +8,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip_cidr] # es. "158.47.227.0/24"
   }
 
   egress {
@@ -18,6 +19,28 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
+# RDS Security Group
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Accesso al database solo dal Bastion"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  egress {
+   from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# RDS Security Group
 resource "aws_security_group" "rds_sg" {
   name        = "rds-sg"
   description = "Accesso al database solo dal Bastion"
