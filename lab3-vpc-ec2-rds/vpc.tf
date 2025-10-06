a006a93803bf2ac5365340ffe5ef8d1c373dc154
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-06 11:50:28 +0200

    Update vpc.tf

--- lab3-vpc-ec2-rds/vpc.tf
+++ lab3-vpc-ec2-rds/vpc.tf
@@ -1,34 +1,44 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  tags = {
-    Name = "lab3-vpc"
-  }
+  tags = { Name = "lab3-vpc" }
 }
 
+# Subnet pubblica per Bastion
 resource "aws_subnet" "public" {
+  vpc_id                   = aws_vpc.main.id
+  cidr_block               = var.public_subnet_cidr
+  map_public_ip_on_launch  = true
+  availability_zone        = "eu-central-1a"
+  tags = { Name = "PublicSubnet" }
+}
+
+# Subnet
+resource "aws_subnet" "private1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr
-  map_public_ip_on_launch = true
+  cidr_block        = "10.0.2.0/24"  # subnet privata 1
   availability_zone = "eu-central-1a"
   tags = {
-    Name = "PublicSubnet"
+    Name = "PrivateSubnet1"
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidr
+  cidr_block        = "10.0.3.0/24"  # subnet privata 2, CIDR diverso!
   availability_zone = "eu-central-1b"
   tags = {
-    Name = "PrivateSubnet"
+    Name = "PrivateSubnet2"
   }
 }
 
+
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags = { Name = "IGW" }
+  tags   = { Name = "IGW" }
 }
 
+# Route table pubblica
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
