fe5751c24ace33c88ac5f7c5172561d3a8667151
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-06 11:49:29 +0200

    Update variables.tf

--- lab3-vpc-ec2-rds/variables.tf
+++ lab3-vpc-ec2-rds/variables.tf
@@ -1,53 +1,8 @@
-variable "aws_region" {
-  description = "Regione AWS"
-  type        = string
-  default     = "eu-central-1"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR della VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidr" {
-  description = "CIDR subnet pubblica"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "private_subnet_cidr" {
-  description = "CIDR subnet privata"
-  type        = string
-  default     = "10.0.2.0/24"
-}
-
-variable "bastion_ami" {
-  description = "AMI per il Bastion Host"
-  type        = string
-  default     = "ami-0c55b159cbfafe1f0" # esempio per Ubuntu 22.04 EU-Central
-}
-
-variable "bastion_instance_type" {
-  description = "Tipo di istanza per Bastion Host"
-  type        = string
-  default     = "t3.micro"
-}
-
-variable "db_username" {
-  description = "Username per RDS MySQL"
-  type        = string
-  default     = "admin"
-}
-
-variable "db_password" {
-  description = "Password per RDS MySQL"
-  type        = string
-  default     = "ChangeMe123!"
-}
-
-variable "db_name" {
-  description = "Nome del database"
-  type        = string
-  default     = "labdb"
-}
+variable "aws_region" { default = "eu-central-1" }
+variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "public_subnet_cidr" { default = "10.0.1.0/24" }
+variable "bastion_ami" { default = "ami-0b104d881e3078545" } # ultima Ubuntu Jammy EU-Central-1
+variable "bastion_instance_type" { default = "t3.micro" }
+variable "db_username" { default = "admin" }
+variable "db_password" { default = "ChangeMe123!" }
+variable "my_ip_cidr" { default = "158.47.227.0/24" } # il tuo IP pubblico
