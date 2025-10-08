1d6f2fa25dde8270124bc3acc46436e8a4f92e2e
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-08 14:58:59 +0200

    Update ec2.tf

--- lab3-vpc-ec2-rds/ec2.tf
+++ lab3-vpc-ec2-rds/ec2.tf
@@ -1,12 +1,13 @@
 resource "aws_instance" "bastion" {
-  ami                    = var.bastion_ami
-  instance_type          = var.bastion_instance_type
-  subnet_id              = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-  key_name               = "lab3-key"
+  ami                    = var.bastion_ami               # ID dell'immagine AMI da usare per la Bastion host, gestita come variabile
+  instance_type          = var.bastion_instance_type      # Tipo di istanza EC2 (esempio: t2.micro), gestito come variabile
+  subnet_id              = aws_subnet.public.id           # ID della subnet pubblica in cui lanciare la Bastion host
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]  # Lista di security group associati all’istanza; qui si fa riferimento al security group specifico per Bastion
+  key_name               = "lab3-key"                     # Nome della key pair usata per SSH sull’istanza
 
-  associate_public_ip_address = true
+  associate_public_ip_address = true                      # Assegna automaticamente un IP pubblico all’istanza per l’accesso remoto
 
-  tags = { Name = "BastionHost" }
+  tags = { Name = "BastionHost" }                         # Tag dell’istanza per identificazione e gestione su AWS (console, cost management, ecc.)
 }
+
 
