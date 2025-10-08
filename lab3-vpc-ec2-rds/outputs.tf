c540209c9e91a81454b57b4437ef8d925fec515c
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-08 14:59:59 +0200

    Update outputs.tf

--- lab3-vpc-ec2-rds/outputs.tf
+++ lab3-vpc-ec2-rds/outputs.tf
@@ -1,12 +1,10 @@
 output "bastion_public_ip" {
-  description = "IP pubblico del Bastion Host"
-  value       = aws_instance.bastion.public_ip
+  description = "IP pubblico del Bastion Host"            # Descrizione esplicativa dell’output, utile per capire cosa restituisce
+  value       = aws_instance.bastion.public_ip            # Valore restituito: l’indirizzo IP pubblico dell’istanza EC2 chiamata "bastion"
 }
 
 output "rds_endpoint" {
-  description = "Endpoint del database RDS"
-  value       = aws_db_instance.mysql.endpoint
+  description = "Endpoint del database RDS"                # Descrizione per identificare chiaramente l’output
+  value       = aws_db_instance.mysql.endpoint             # Valore restituito: hostname/endpoint dell’istanza DB RDS denominata "mysql"
 }
-
-
 
