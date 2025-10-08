bdd8012ec74bdd94512e2b7469c3fa35411421b2
Ferramelo <carmelo.ferraro991@gmail.com>
2025-10-08 14:57:40 +0200

    Update backend.tf

--- lab3-vpc-ec2-rds/backend.tf
+++ lab3-vpc-ec2-rds/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "lab3-terraform-state211289420968"
-    key    = "terraform.tfstate"
-    region = "eu-central-1"
-    use_lockfile = true
+    bucket         = "lab3-terraform-state211289420968" # Nome del bucket S3 che ospita il file di stato
+    key            = "terraform.tfstate"                # Percorso/nome del file di stato all’interno del bucket
+    region         = "eu-central-1"                     # Regione AWS del bucket S3 e della tabella DynamoDB
+    dynamodb_table = "lab3-terraform-lock"              # Tabella DynamoDB utilizzata per il locking distribuito
   }
 }
 
